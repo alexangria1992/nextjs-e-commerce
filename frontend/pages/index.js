f21d475d76177bcc880057236bb6f48a7fe2306d
alexangria1992 <alex040892@gmail.com>
2023-04-02 21:13:38 +1200

    Homepage Product Listing

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,5 @@
 import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
@@ -18,7 +19,22 @@ export default function Home() {
               running.
             </div>
           </div>
-          {/*  Heading and paragraph start */}
+          {/*  Heading and paragraph end */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            <ProductCard />
+            <ProductCard />
+
+            <ProductCard />
+
+            <ProductCard />
+
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
         </Wrapper>
       </main>
     </>
