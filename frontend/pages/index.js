e6212da4216e69e7160f132a9736c74e4c7cb59d
alexangria1992 <alex040892@gmail.com>
2023-04-02 13:42:55 +1200

    Heading and Paragraph

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,25 @@
 import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
     <>
       <main>
         <HeroBanner />
+        <Wrapper>
+          {/* Heading and Paragraph Start */}
+          <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px] ">
+            <div className="text-[28px] mb-5 font-semibold leading-right md:text-[34px]">
+              Cushioning Your Miles
+            </div>
+            <div className="text-md md:text-xl">
+              A lightweight Nike zoomX midsole is combined with increased stack
+              heights to help provide cushioning during extended stretches of
+              running.
+            </div>
+          </div>
+          {/*  Heading and paragraph start */}
+        </Wrapper>
       </main>
     </>
   );
