c55dc1a8b8310643df7d841610e9f4d4903e22a9
alexangria1992 <alex040892@gmail.com>
2023-04-04 19:45:47 +1200

    Homepage products data mapping and fetching

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,24 +2,25 @@ import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [data, setData] = useState(null);
+export default function Home({ products }) {
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
 
-  const fetchProducts = async () => {
-    const { data } = await fetchDataFromApi("/api/products");
-    setData(data);
-  };
+  // const fetchProducts = async () => {
+  //   const { data } = await fetchDataFromApi("/api/products");
+  //   setData(data);
+  // };
 
   return (
     <>
       <main>
         <HeroBanner />
+        <h1>{products?.data?.[0]?.attributes?.name}</h1>
         <Wrapper>
           {/* Heading and Paragraph Start */}
           <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px] ">
@@ -36,6 +37,10 @@ export default function Home() {
 
           {/* Products Grid Start */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            {products?.data?.map((product) => (
+              <ProductCard key={product?.id} data={product} />
+            ))}
+            {/* <ProductCard />
             <ProductCard />
             <ProductCard />
             <ProductCard />
@@ -44,12 +49,19 @@ export default function Home() {
             <ProductCard />
             <ProductCard />
             <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <ProductCard /> */}
           </div>
           {/* Products Grid Ends */}
         </Wrapper>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
