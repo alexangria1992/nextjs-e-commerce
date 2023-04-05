787636151bcad4a8d258f136dc504f534f0307a1
alexangria1992 <alex040892@gmail.com>
2023-04-05 21:52:26 +1200

    Pagination functionality

--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -1,12 +1,30 @@
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 
-import React from "react";
+const maxResult = 3;
 
 const Category = ({ category, products, slug }) => {
+  const [pageIndex, setPageIndex] = useState(1);
+  const { query } = useRouter();
+
+  useRouter(() => {
+    setPageIndex(1);
+  }, [query]);
+
+  const { data, error, isLoading } = useSWR(
+    `/api/products?populate=*&[filters]
+  [categories][slug][$eq]=${slug}&pagination[page]=${pageIndex}&pagination[pageSize]=${maxResult}`,
+    fetchDataFromApi,
+    {
+      fallbackData: products,
+    }
+  );
   return (
-    <div className="w-full md:py-20  border-pink-500 border-2 border-solid ">
+    <div className="w-full md:py-20  border-pink-500 border-2 border-solid relative">
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
@@ -15,10 +33,43 @@ const Category = ({ category, products, slug }) => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          {products?.data?.map((product) => (
+          {data?.data?.map((product) => (
             <ProductCard key={product?.id} data={product} />
           ))}
         </div>
+
+        {/* PAGINATION BUTTONS START */}
+
+        {data?.meta?.pagination?.total > maxResult && (
+          <div className="flex gap-3 items-center justify-center my-16 md:my-0">
+            <button
+              className={`rounded py-2 px-4 bg-black text-white
+             disabled:bg-gray-200 disbaled:text-gray-500`}
+              disabled={pageIndex === 1}
+              onClick={() => setPageIndex(pageIndex - 1)}
+            >
+              Previous
+            </button>
+            <span className="font-bold">{`${pageIndex} of ${
+              data && data.meta.pagination.pageCount
+            }`}</span>
+            <button
+              className={`rounded py-2 px-4 bg-black text-white
+             disabled:bg-gray-200 disbaled:text-gray-500`}
+              disabled={pageIndex === (data && data.meta.pagination.pageCount)}
+              onClick={() => setPageIndex(pageIndex + 1)}
+            >
+              Next
+            </button>
+          </div>
+        )}
+        {/* PAGINATION BUTTONS END */}
+        {isLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-white/[0.5] flex flex-col justify-center items-center">
+            <img src="/logo.svg" alt="" width={150} />
+            <span className="text-2xl font-medium">Loading...</span>
+          </div>
+        )}
       </Wrapper>
     </div>
   );
@@ -47,7 +98,7 @@ export async function getStaticProps({ params: { slug } }) {
   );
   const products =
     await fetchDataFromApi(`/api/products?populate=*&[filters][categories]
-[slug][$eq]=${slug}`);
+[slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}`);
 
   return {
     props: {
